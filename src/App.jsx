cf462351023731a763aefc255dc2f2c8eebff44c
Covenant Ogowale <justcovenant@outlook.com>
2024-03-08 13:26:32 +0100

    finsihed registration page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Event from "./pages/Event";
+import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./styles/global.css";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<PageLayout />}>
             <Route index element={<Home />} />
             <Route path="/event" element={<Event />} />
+            <Route path="/event/register" element={<Register />} />
           </Route>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<SignUp/>}></Route>
---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import { Box, Text, Input, Button } from "@chakra-ui/react"
+
+const Register = () => {
+  return (
+    <>
+      <Box>
+        <Text fontSize={"5xl"} textAlign={"center"}>Registration</Text>
+        <Text textAlign={"center"}>Register to Dream WorldWide In Abuja</Text>
+        <Box display={"flex"} justifyContent={"center"} mt={8}>
+          <form>
+          <Box>
+              <label htmlFor="name">Your Name:</label>
+              <Input border={"1px solid blue"} type="text" name="name" id="name"/>
+            </Box>
+            <Box mt={3}>
+              <label htmlFor="email">Your Email:</label>
+              <Input border={"1px solid blue"} type="email" name="email" id="email"/>
+            </Box>
+            <Button width={"100%"} fontWeight={100} mt={3} backgroundColor={"#7848F4"} color={"white"} _hover={{backgroundColor: "#7848F4", color:"white"}}>Submit</Button>
+          </form>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default Register
