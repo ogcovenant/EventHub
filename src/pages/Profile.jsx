c231c100c10161e2cd92d50062ddeaeb28b321d6
Covenant Ogowale <justcovenant@outlook.com>
2024-03-12 20:30:08 +0100

    finished profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,10 +4,9 @@ const Profile = () => {
   return (
     <>
       <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-        <Text fontSize={38}>Profile</Text>
-        <Box mt={5}>
+        <Box mt={16}>
           <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-            <Box width={"60px"} height={"60px"} backgroundColor={"white"} borderRadius={"50%"}></Box>
+            <Box width={"60px"} height={"60px"} backgroundColor={"black"} borderRadius={"50%"}></Box>
             <Text mt={1} color={"blue"} cursor={"pointer"}>Change Avatar</Text>
           </Box>
           <Box mt={6}>
@@ -15,16 +14,16 @@ const Profile = () => {
               <Box display={"flex"} gap={"20px"}>
                 <Box>
                   <label htmlFor="username">username</label>
-                  <Input type="text" name="username" id="username" placeholder="john_doe" border={"1px solid grey"}/>
+                  <Input type="text" name="username" id="username" placeholder="john_doe" value={"just_covenant_"} border={"1px solid grey"}/>
                 </Box>
                 <Box>
                   <label htmlFor="email">email</label>
-                  <Input type="email" name="email" id="email" placeholder="john@doe.com" border={"1px solid grey"}/>
+                  <Input type="email" name="email" id="email" placeholder="john@doe.com" value={"justcovenant@gmail.com"} border={"1px solid grey"}/>
                 </Box>
               </Box>
               <Box mt={5}>
                 <label htmlFor="password">password</label>
-                  <Input type="password" name="password" id="password" placeholder="👻👻👻👻👻👻👻👻" border={"1px solid grey"}/>
+                  <Input type="password" name="password" id="password" value={"12345678"} placeholder="👻👻👻👻👻👻👻👻" border={"1px solid grey"}/>
               </Box>
               <Box textAlign={"center"}>
                 <Text as={"button"} mt={"8"} type="submit" color={"blue"}> Save Changes </Text>
