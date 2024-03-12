e2bc1e89bb3e374194a16d75f3b76697f813c362
Covenant Ogowale <justcovenant@outlook.com>
2024-03-12 14:05:33 +0100

    finished solo event page responsiveness

--- src/components/EventDisplayCard.jsx
+++ src/components/EventDisplayCard.jsx
@@ -5,12 +5,11 @@ import { Link } from "react-router-dom"
 const EventDisplayCard = () => {
   return (
     <>
-      <Box mt={7} boxShadow={"2xl"} width={{ base:"100%", md:"45%", lg:"23%" }} p={5} borderRadius={15} backgroundColor={"white"} cursor={"pointer"}>
+      <Box mt={7} boxShadow={"2xl"} width={{ base:"100%", sm:"45%", lg:"23%" }} p={5} borderRadius={15} backgroundColor={"white"} cursor={"pointer"}>
         <Link to={"/event"}>
           <Flex flexDirection={"column"}>
             <Box position={"relative"} width={"100%"}>
               <Image as={"img"} src={eventCard} width={"100%"}/>
-              <Text position={"absolute"} top={2} left={2} color={"#7848F4"} backgroundColor={"white"} p={1} borderRadius={5} fontSize={13}>FREE</Text>
             </Box>
             <Text mt={3} fontSize={17} className="text">BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</Text>
             <Text mt={5} fontSize={14}>Saturday, March 18, 9.30PM</Text>
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Button, Heading, Flex, Text } from "@chakra-ui/react"
+import { Box, Image, Button, Flex, Text } from "@chakra-ui/react"
 import eventHero from "../assets/eventHero.png"
 import { Link } from "react-router-dom"
 import { FaArrowLeft } from "react-icons/fa";
@@ -6,28 +6,22 @@ import { FaArrowLeft } from "react-icons/fa";
 const Event = () => {
   return (
     <>
-      <Box mt={5} display={"flex"} justifyContent={"center"} position={"relative"}>
-        <Box width={"90%"}>
-          <Image as={"img"} src={eventHero} width={"100%"} height={"80vh"}/>
-          <Box height={"100%"} width={"90%"} backgroundColor={"#000000af"} position={"absolute"} top={0} borderRadius={10} display={"flex"} p={8}>
-            <Flex flexDirection={"column"} alignItems={"space-around"} width={"50%"}>
-              <Box>
-                <Link to="/"><Button display={"flex"} gap={1}><FaArrowLeft /> Back</Button></Link>
-              </Box>
-              <Box>
-                <Heading fontSize={40} mt={8} color={"white"}>Dream WorldWide In Abuja</Heading>
-                <Text color={"white"}>By Covenant Ogowale</Text>
-              </Box>
-              <Box color={"white"} mt={16}>
-                <Text fontSize={32}>Abuja, Nigeria</Text>
-                <Text mt={3}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ratione laboriosam vero officia. Itaque, repellat harum veritatis eveniet cupiditate temporibus sint similique, esse rem vitae deserunt doloribus dolor beatae voluptate?
-                </Text>
-              </Box>
-              <Link to={"/event/register"}><Button fontWeight={100} mt={2} backgroundColor={"#7848F4"} color={"white"} _hover={{backgroundColor: "#7848F4", color:"white"}}>Register for this event</Button> </Link>
-            </Flex>
+      <Box mt={5} position={"relative"} my={{ base:5, md:20 }}>
+      <Link to={"/"}><Button mx={10} backgroundColor={"black"} color={"white"} fontWeight={100}><FaArrowLeft />&nbsp;Back</Button></Link>
+        <Flex p={10} alignItems={"center"} flexDirection={{ base:"column", lg:"row" }}>
+          <Box width={"95%"} position={"relative"}>
+            <Image as={"img"} borderRadius={20} src={eventHero} width={"100%"} height={"40vh"} objectFit={"cover"}/>
           </Box>
-        </Box>
+          <Flex m={5}>
+            <Box>
+              <Text fontWeight={700} fontSize={32}>Dream Wold</Text>
+              <Text mt={1}>By Covenant Ogowale</Text>
+              <Text>Saturday 12th July, 2024</Text>
+              <Text mt={3} textAlign={"justify"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint mollitia doloribus, velit voluptates necessitatibus possimus veritatis labore pariatur inventore quis ea, repellat animi obcaecati, reiciendis sed quas perspiciatis eligendi quos!</Text>
+              <Link to="/event/register"><Button fontWeight={"100"} backgroundColor={"#7848F4"} color={"white"} _hover={{backgroundColor: "#7848F4", color:"white"}} mt={3}>Register to this event</Button></Link>
+            </Box>
+          </Flex>
+        </Flex>
       </Box>
     </>
   )
