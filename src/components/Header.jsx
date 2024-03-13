003ab5609d7f942ea429e9931fc8f194e9ae2349
Covenant Ogowale <justcovenant@outlook.com>
2024-03-13 10:21:15 +0100

    added login and logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
-import { Flex, Image, Box, Button, Text } from "@chakra-ui/react"
+import { Flex, Image, Box, Button, Text, useToast } from "@chakra-ui/react"
 import logo from "../assets/logo.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { FaBars} from "react-icons/fa"
 import { FaXmark } from "react-icons/fa6";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useLockScroll } from './useLockBodyScroll'
 
 const Header = () => {
+  
+  const navigate = useNavigate();
+  const toast = useToast();
+  const [token, setToken] = useState(localStorage.getItem("token"))
+
+
+  useEffect(() => {
+    navigate("/", { replace: true })
+    
+    if(!token){
+      toast({
+        title: `Logout Successful`,
+        status: "success",
+        position: "top-right",
+        variant: "left-accent",
+        isClosable: true,
+      })
+    }
+
+  }, [token])
 
   const [menuOpen, setMenuOpen] = useState(false);
   const openMenu = () => {
@@ -19,17 +39,38 @@ const Header = () => {
 
   useLockScroll(menuOpen)
 
+  const logout = () => {
+    localStorage.removeItem("token")
+    setToken("")
+  }
+
   return (
     <>
       <Flex p={5} justifyContent={"space-between"} alignItems={"center"} >
         <Link to={"/"}><Image as={'img'} src={logo} width={150}/></Link>
-        <Box display={{ base:"none", md:"flex" }} gap={8} alignItems={"center"}>
-          <Text as={Link} to="/login" color={"#7848F4"}>Login</Text>
-          <Button as={Link} to="/signup" fontWeight={100} backgroundColor={"#7848F4"} color={"white"} _hover={{backgroundColor: "#7848F4", color:"white"}}>Register</Button>
-        </Box>
-        <Box onClick={openMenu} display={{ md:"none" }}>
-          <FaBars fontSize={22}/>
-        </Box>
+        {
+          !token && 
+            <Box>
+              <Box display={{ base:"none", md:"flex" }} gap={8} alignItems={"center"}>
+              <Text as={Link} to="/login" color={"#7848F4"}>Login</Text>
+              <Button as={Link} to="/signup" fontWeight={100} backgroundColor={"#7848F4"} color={"white"} _hover={{backgroundColor: "#7848F4", color:"white"}}>Register</Button>
+            </Box>
+            <Box onClick={openMenu} display={{ md:"none" }}>
+              <FaBars fontSize={22}/>
+            </Box>
+          </Box>
+        }
+        {
+          token && 
+          <Box display={"flex"} gap={5} alignItems={"center"}>
+            <Text as={"button"} onClick={logout} color={"red"}>Logout</Text>
+            <Link to={"/dashboard"}>
+              <Box width={"40px"} height={"40px"} backgroundColor={"black"} borderRadius={"50%"}>
+              
+              </Box>
+            </Link>
+          </Box>
+        }
       </Flex>
       {
         menuOpen && 
