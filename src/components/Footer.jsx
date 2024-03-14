4bd317d8b54817b7e437ccb3651287bd0fa0de7d
Covenant Ogowale <justcovenant@outlook.com>
2024-03-14 23:42:47 +0100

    finished profile page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,11 @@
 import logo from "../assets/logo2.png"
-import { Image, Button, Input, Box, Text } from "@chakra-ui/react"
+import { Image, Box, Text } from "@chakra-ui/react"
 
 const Footer = () => {
   return (
     <>
       <Box mt={10} backgroundColor={"#10107B"} p={10} display={"flex"} flexDirection={"column"} alignItems={"center"}>
         <Image as={"img"} src={logo} width={120}/>
-        <Text color={"white"} mt={5}>Subscribe to our newsletter:</Text>
-        <form style={{ width: "100%", margin: "0 auto", display:"flex", justifyContent:"center", alignItems:"center" }}>
-            <Input backgroundColor={"white"} width={{ base:"70%", md:"50%" }} m={1} placeholder="Enter Your Email" />
-            <Button type="submit" width={{ base:"30%", md:"15%" }} mx={2}>Subscribe</Button>
-        </form>
         <Text color={"white"} mt={5}>©2024 Covenant Ogowale </Text>
       </Box>
     </>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,70 +1,111 @@
 import { Box, Text, Input, Image } from "@chakra-ui/react"
-import { useState } from "react";
+import { useState,useEffect, useRef } from "react";
 import { useFormik } from "formik"
 import * as Yup from "yup"
 
 const Profile = () => {
-  const [ file,setFile] = useState();
-  function handleChange(e) {
-    console.log(e.target.files)
-    setFile(URL.createObjectURL(e.target.files[0]))
+  const [ filePath,setFilePath] = useState();
+  const [file, setFile] = useState(null)
+  const [ user ,setUser ] = useState([]);
+  const formRef = useRef(null)
 
+  const token = localStorage.getItem("token");
+
+
+  const fetchUser = async () => {
+    const result = await fetch("http://localhost:3000/profile", {
+        headers: {
+          "authorization" : `Bearer ${token}`
+        }
+    })
+    const user = await result.json();
+    setUser(user.user[0])
+    console.log(user.user[0])
+  }
+
+  useEffect(() =>{
+
+    fetchUser();
+
+  },[])
+ 
+
+  const handleSubmit = async (e) => {
+
+      const formData = new FormData(e.current);
+      formData.append("profilePic", file)
+      const res = await fetch("http://localhost:3000/profile/edit", {
+        method: "PUT",
+        headers: {
+          "authorization": `Bearer ${token}`
+        },
+        body: formData
+
+      })
+    
+      const msg = await res.json();
+      console.log(msg)
+  }
+
+  function handleChange(e) {
+    setFilePath(URL.createObjectURL(e.target.files[0]))
+    setFile(e.target.files[0])
   }
  
   const formik = useFormik({
     initialValues:{
-      username: "just_covenant_",
-      email: "justcovenant@gmail.com",
-      password: "Covenant@07",
+      username: user.username,
+      email: user.email,
+      password: "",
     },
+    enableReinitialize: true,
     validationSchema: Yup.object({
       username: Yup.string().min(3, "username can not be less than 3 characters"),
       email: Yup.string().email("Invalid email address"),
       password: Yup.string().min(8, "Password can not be less than 8 characters"),
-    }),
-    onSubmit: async (values) => {
-      console.log(values);
+    }), 
+    onSubmit: () => {
+      handleSubmit(formRef);
     }
   })
 
   return (
     <>
       <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-      <form onSubmit={formik.handleSubmit}>
         <Box mt={16}>
           <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
             <Box width={"100px"} height={"100px"} backgroundColor={"black"} borderRadius={"50%"}>
-              <Image width={"100%"} height={"100%"} objectFit={"cover"} borderRadius={"50%"} src={file}/>
-              
+              <Image width={"100%"} height={"100%"} objectFit={"cover"} borderRadius={"50%"} src={user.profilePic}/>       
             </Box>
             <Text as={"label"} htmlFor="profilePic" mt={1} color={"blue"} cursor={"pointer"}>Change Avatar</Text>
-            <Input onChange={handleChange} type="file" display={"none"} name="profilePic" id="profilePic" accept="image/*"/>
+            <Input as={"input"} onChange={handleChange} type="file" display={"none"} name="profilePic" id="profilePic" accept="image/*"/>
 
           </Box>
           <Box mt={6}>
+            <form ref={formRef} onSubmit={formik.handleSubmit}>
             <Box display={"flex"} gap={{ base:"5px", md:"20px" }}>
               <Box width={"50%"}>
                 <label htmlFor="username">username</label>
-                <Input type="text" name="username" id="username" placeholder="john_doe" value={formik.values.username} onBlur={formik.handleBlur} onChange={formik.handleChange} border={"1px solid grey"}/>
+                <Input as={"input"} type="text" name="username" id="username" placeholder="john_doe" value={formik.values.username} onBlur={formik.handleBlur} onChange={formik.handleChange} border={"1px solid grey"}/>
                 {formik.errors.username && formik.touched.username ? (<Text color={"red"} fontSize={12}>{formik.errors.username}</Text>) : null}
               </Box>
               <Box width={"50%"}>
                 <label htmlFor="email">email</label>
-                <Input type="email" name="email" id="email" placeholder="john@doe.com" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} border={"1px solid grey"}/>
+                <Input as={"input"} type="email" name="email" id="email" placeholder="john@doe.com" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} border={"1px solid grey"}/>
                 {formik.errors.email && formik.touched.email ? (<Text color={"red"} fontSize={12}>{formik.errors.email}</Text>) : null}
               </Box>
             </Box>
             <Box mt={5}>
               <label htmlFor="password">password</label>
-              <Input type="password" name="password" id="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder="👻👻👻👻👻👻👻👻" border={"1px solid grey"}/>
+              <Input as={"input"} type="password" name="password" id="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder="👻👻👻👻👻👻👻👻" border={"1px solid grey"}/>
               {formik.errors.password && formik.touched.password ? (<Text color={"red"} fontSize={12}>{formik.errors.password}</Text>) : null}
             </Box>
             <Box textAlign={"center"}>
               <Text as={"button"} mt={"8"} type="submit" color={"blue"}> Save Changes </Text>
             </Box>
+          </form>
           </Box>
         </Box>
-      </form>
       </Box>
     </>
   )
